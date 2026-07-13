4372f33eb7aae9ad91513dc7f5d8478083126c43
mshajkarami <mshajkarami.dev@gmail.com>
2026-07-13 23:53:08 +0330

    Update Task File

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -104,6 +104,8 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    implementation(libs.androidx.core.splashscreen)
+
 
     // Coil
     implementation("io.coil-kt:coil-compose:2.7.0")
--- app/src/main/java/dev/mshajkarami/memocraft/app/MainActivity.kt
+++ app/src/main/java/dev/mshajkarami/memocraft/app/MainActivity.kt
@@ -4,14 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dagger.hilt.android.AndroidEntryPoint
 import dev.mshajkarami.memocraft.core.designsystem.theme.MemoCraftAppTheme
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
+
         setContent {
             MemoCraftAppTheme {
                 MemoCraftApp()
